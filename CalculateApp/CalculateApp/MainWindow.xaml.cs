63eea15edadb743f46d7cb19c4229b4036ae149d
Eww48 <pusaming.rac@spumail.net>
2021-04-16 15:43:21 +0700

    Added main files
    test

---
+++ CalculateApp/CalculateApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculateApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int income;
+        int expense;
+        float minus;
+        int price;
+        float result;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            income = int.Parse(txtIncome.Text);
+            expense = int.Parse(txtExpenses.Text);
+            price = int.Parse(txtPrice.Text);
+            minus = income - expense;
+            result = price / minus;
+            txtDays.Text = result.ToString();
+        }
+    }
+}
